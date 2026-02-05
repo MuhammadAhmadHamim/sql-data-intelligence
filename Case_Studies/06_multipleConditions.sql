119d3a8886b6418d3197327f352f254f840fa3a7
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-05 15:41:38 +0500

    Query where more than one conditions are implemented!

--- Case_Studies/06_multipleConditions.sql
+++ Case_Studies/06_multipleConditions.sql
@@ -8,3 +8,15 @@ SELECT name, color
 FROM candies
 -- Write your code below
 WHERE color="brown" AND price=2 OR color="mixed" AND weight < 40
+
+--Fetch all of the people who meet one of the following criteria:
+
+--1)The age is above 30 and the status either is seeking or retired
+--2)The age is under 50 and the status either is employed or unemployed
+--3)The age is between 20 and 28 (including 20 and 28)
+
+SELECT *
+FROM people
+WHERE age> 30 AND (status="retired" OR status="seeking")
+OR age< 50 AND (status="employed" OR status="unemployed")
+OR (age> 19 AND age<29)
