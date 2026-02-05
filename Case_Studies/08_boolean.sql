6e9d6b6c10008dca389deacddac9f7796fbdafda
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-05 15:36:40 +0500

    Checked the working of Boolean values...

--- Case_Studies/08_boolean.sql
+++ Case_Studies/08_boolean.sql
@@ -3,3 +3,12 @@
 Select *
 FROM objects
 WHERE colourful IS TRUE
+
+--Find all objects that meet AT LEAST ONE of these conditions:
+
+--1)colorful and not fragile
+--2)In stock and small (size <= 2)
+SELECT *
+FROM objects
+WHERE (colorful IS TRUE AND is_fragile IS NOT TRUE) 
+OR in_stock IS TRUE AND size<= 2
