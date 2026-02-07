fa2cbba07128e58b18cfdf8c63522acef12d5b49
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-07 20:20:56 +0500

    Another recap this time through a Vehicle Factory case..

---
+++ Case_Studies/13_vehiclefactory.sql
@@ -0,0 +1,10 @@
+--As an owner of a vehicle factory, you have agreed to provide a salary raise for the four employees with the lowest salaries 
+--who are also married,as they are struggling to finance their families. 
+--Return only the IDs of the relevant employees. 
+--Sort the results by salary in ascending order.
+SELECT DISTINCT id
+FROM employees
+WHERE status="married"
+AND salary< 5903
+ORDER BY salary
+LIMIT 4
