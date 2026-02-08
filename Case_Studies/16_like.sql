721c47a911ea0e65ef7947211762b96585d0c626
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-08 22:59:19 +0500

    Time for the LIKE keyword..

---
+++ Case_Studies/16_like.sql
@@ -0,0 +1,6 @@
+--Fetch all of the people whose name starts with K (uppercase)
+--and ends with a (lowercase) and order the results by the names in descending order.
+SELECT *
+FROM people
+WHERE name LIKE 'K%a'
+ORDER BY name DESC 
