1b0da5c51303b084039085b960d05649616b5436
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-11 23:37:06 +0500

    The Modulo Operator..

---
+++ Case_Studies/21_modulooperator.sql
@@ -0,0 +1,9 @@
+--Create a query that:
+
+--Shows the product ID
+--Assigns each product to one of three quality control teams (0, 1, or 2) based on the product ID using the modulo operator,
+--call this column as quality_control
+--Only includes products where the quantity column contains an odd number (e.g., 1, 3, 5, 7...)
+SELECT id,id % 3 AS quality_control
+FROM products 
+WHERE quantity % 2 = 1
