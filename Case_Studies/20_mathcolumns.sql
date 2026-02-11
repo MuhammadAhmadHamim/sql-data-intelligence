09a15658c6f4e639f7441bc6228e6984e4e91363
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-11 23:36:33 +0500

    Juggling between some math columns..

---
+++ Case_Studies/20_mathcolumns.sql
@@ -0,0 +1,11 @@
+--Create a query that calculates the following:
+
+--A column called total_value that multiplies the price by quantity and adds a fixed shipping cost of 15
+--A column called discounted_value that:
+--First multiplies the price by quantity
+--Then applies a 20% discount (multiply by 0.8)
+--Finally adds a fixed shipping cost of 10
+--Try to use parentheses appropriately to ensure the calculations are performed in the correct order!
+SELECT (price * quantity) + 15 AS total_value,
+       ((price * quantity) * 0.8) + 10 AS discounted_value
+FROM products;
