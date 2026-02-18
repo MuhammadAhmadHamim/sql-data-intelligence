b4228bdcef1f353f831b3741d0ab48ade64662c2
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-18 20:52:45 +0500

    Juggling through some basic built-In functions of SQL(nested-queries)..

---
+++ SQL_Fundamentals/Aggregate_Functions/02_builtInAggregate.sql
@@ -0,0 +1,11 @@
+/*
+Calculate how much each item's price exceeds the average price of all items. 
+Display the item ID, its price, and the difference from the average.
+Name the difference column as diff_from_avg.
+
+Order the results by the difference in descending order.
+*/
+SELECT id,price,
+       price - (SELECT AVG(price) FROM items) AS diff_from_avg
+FROM items
+ORDER BY diff_from_avg DESC
