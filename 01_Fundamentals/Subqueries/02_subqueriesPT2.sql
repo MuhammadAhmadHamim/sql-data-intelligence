016944eca8300793c16e1d2693183665305d4502
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-22 19:24:57 +0500

    Time to juggle a bit in the subqueries section(PT 2)

--- 01_Fundamentals/Subqueries/02_subqueriesPT2.sql
+++ 01_Fundamentals/Subqueries/02_subqueriesPT2.sql
@@ -6,3 +6,11 @@ Steps to solve:
 (i)  First, we need the average of ALL quantities (This is the subquery)
 (ii) Then, for each category, sum their quantities and compare with this average (use the HAVING keyword for group by)
 */
+SELECT category,
+SUM(quantity) AS total_quantity
+FROM shop
+GROUP BY category
+HAVING total_quantity > (
+    SELECT AVG(quantity)
+    FROM shop
+);
