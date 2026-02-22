f6c819b484e3080f93d44c2e2b4b686622bf57b2
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-22 19:17:24 +0500

    Time to juggle a bit in the subqueries section(PT 1)

---
+++ 01_Fundamentals/Subqueries/01_subqueriesPT1.sql
@@ -0,0 +1,11 @@
+/*
+Write a query to find all items (select all columns) where the price is higher than the average price of all items.
+
+Use a subquery in the WHERE clause to calculate the average price, then compare each item's price against this average.
+*/
+SELECT *
+FROM shop
+WHERE price > (
+    SELECT AVG(price)
+    FROM shop
+);
