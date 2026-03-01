44d8c2af9eec98297ed4349290a0c6b3665fbf70
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-02 02:20:22 +0500

    Date functions (PT4)

---
+++ 01_Fundamentals/Date_Functions/04_dateoperations4.sql
@@ -0,0 +1,10 @@
+/*
+Your task is to print the current date and time.
+Then, print the current date only.
+After that you eek out the current day from current date value.
+*/
+SELECT NOW();
+
+SELECT CURDATE();
+
+SELECT DAY(CURDATE());
