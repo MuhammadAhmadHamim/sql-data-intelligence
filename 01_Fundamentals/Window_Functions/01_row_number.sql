8aef0f47c0b8d693ca8771d7672e5b5b6175f948
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-04-20 00:12:42 +0500

    Using ROW_NUMBER() to go through rows without collapsing them for multiple tables

--- 01_Fundamentals/Window_Functions/01_row_number.sql
+++ 01_Fundamentals/Window_Functions/01_row_number.sql
@@ -7,3 +7,26 @@ SELECT id, density,
     ROW_NUMBER() OVER() AS row_num
 FROM liquids
 WHERE density > 5.677;
+
+/*
+For each department:
+
+Assign a row number to students based on marks (highest first)
+*/
+SELECT name, dept, marks,
+    ROW_NUMBER() OVER(PARTITION BY dept ORDER BY marks DESC) AS rank
+FROM Students;
+
+/*
+Same Table.
+
+Get only the top student from each department.
+*/
+WITH Ranked_students AS (
+    SELECT name, dept, marks
+        ROW_NUMBER() OVER(PARTITION BY dept ORDER BY marks DESC) as rank
+    FROM Students
+)
+SELECT *
+FROM Ranked_students
+WHERE rank = 1;
