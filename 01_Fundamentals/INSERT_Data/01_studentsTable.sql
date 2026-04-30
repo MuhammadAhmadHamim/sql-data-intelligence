4aa61ed60706c8e0108b8901db0ef2ec84827deb
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-04-30 18:55:36 +0500

    Failed case added just to study the constraints better

--- 01_Fundamentals/INSERT_Data/01_studentsTable.sql
+++ 01_Fundamentals/INSERT_Data/01_studentsTable.sql
@@ -13,11 +13,11 @@ CREATE TABLE Student (
     dept VARCHAR(10) DEFAULT 'CS',
     -- FLOAT to allow decimal values 
     cgpa FLOAT CHECK (cgpa BETWEEN 0.0 AND 4.0) 
-    
 );
 
 INSERT INTO Student (student_id, name, dept, cgpa)
 VALUES 
     (1, 'Ace', DEFAULT, 3.9), -- Default case = 'CS'
     (2, 'Luffy', 'CE', 3.5), 
-    (3, 'Zoro', 'ME', 4.0); -- Lowered to 4.0 to pass the CHECK constraint since cgpa can't be above 4.0 
+    (3, 'Zoro', 'ME', 4.0), -- Lowered to 4.0 to pass the CHECK constraint since cgpa can't be above 4.0
+    (4, 'Sanji', 'CE', 4.5); -- ❌ Failed case added just to study the constraints better 
